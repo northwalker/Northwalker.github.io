1688914b7fe3e184c1d26aec741c12429c0a06a9
Northwalker <northwalker@hotmail.com>
2016-07-17 17:37:26 +0800

    Update main & index.

--- main/js/main.js
+++ main/js/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   'use strict';
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('particles-js', './particlesjs-config.json', function () {
@@ -9,20 +9,57 @@
   smoothScroll.init();
 
   var $window = $(window);
-  $(window).scroll(function(){
-    if($window.scrollTop() > 150)
-      $(".navbar-nw").addClass("navbar-nw-shadow");
-    else
-      $(".navbar-nw").removeClass("navbar-nw-shadow");
 
-    if($window.scrollTop() > 800)
-      $(".navbar-nw").addClass("navbar-nw-shadow-2x");
+  $(window).scroll(function () {
+    if ($window.scrollTop() > 100)
+      $(".navigation-nw").addClass("navbar-nw-shadow");
     else
-      $(".navbar-nw").removeClass("navbar-nw-shadow-2x");
+      $(".navigation-nw").removeClass("navbar-nw-shadow");
 
+    if ($window.scrollTop() > 800)
+      $(".navigation-nw").addClass("navbar-nw-shadow-2x");
+    else
+      $(".navigation-nw").removeClass("navbar-nw-shadow-2x");
   });
 
+  $(window).resize(function () {
+    if ($window.width() > 768) {
+      $("#navigation-btn").hide();
+      $(".navigation-nw-list").show();
+    }
+    if ($window.width() < 768) {
+      $("#navigation-btn").show();
+      $(".navigation-nw-list").hide();
+    }
+  });
 
+  $('#navigation-btn').on('click', function () {
+    // $('#navigation-nw-list').show();
+    if ($('.navigation-nw-list').css("display") == 'none') {
+      $(".navigation-nw-list").show("slow", function () {
+        //
+      });
+    }
+    else {
+      $(".navigation-nw-list").hide("slow", function () {
+        //
+      });
+    }
+  });
+
+  function initNavigation() {
+
+    if ($window.width() < 768) {
+      $(".navigation-nw-list").hide();
+      $("#navigation-btn").show();
+    }
+    else {
+      $("#navigation-btn").hide();
+      $(".navigation-nw-list").show();
+    }
+  }
+
+  initNavigation();
 
   function initMap() {
     var myLatLng = {lat: 25.058465, lng: 121.5548159};
@@ -42,11 +79,9 @@
       title: 'Hello! Northwalker!'
     });
   }
+
   initMap();
 
-
   var wow = new WOW().init();
-
-
 
 })();
