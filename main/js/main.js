2b19eace96b2902714b2f14d13d078090f0d4a84
Northwalker <northwalker@hotmail.com>
2016-07-17 17:42:06 +0800

    Update particlesjs

--- main/js/main.js
+++ main/js/main.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', './particlesjs-config.json', function () {
+  particlesJS.load('particles-js', './main/vendor/particlesjs/particlesjs-config.json', function () {
     console.log('callback - particles.js config loaded');
   });
 
