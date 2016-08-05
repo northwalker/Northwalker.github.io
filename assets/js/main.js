18aaf0f079011a5f987cb3fab62e8dc239a162e5
Northwalker <northwalker@hotmail.com>
2016-08-05 23:51:30 +0800

    Update fonts and remove some js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,22 @@
 (function () {
   'use strict';
 
+  var $window = $(window);
+  var viewportHeight = $(window).height();
+  var particlesHeight = $('#particles-js').height();
+  if (particlesHeight < viewportHeight) {
+    // Dynamic update height
+    $('#particles-js').height(viewportHeight);
+  }
+
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function () {
     // console.log('callback - particles.js config loaded');
   });
 
-  $('.goToHome').click(function(){
-    // $body.animate({
-    //   scrollTop: 0
-    // }, 600);
-    $('html, body').animate({
-      scrollTop: $("#Home").offset().top
-    }, 600);
-
-    return false;
-  });
-  $('.goToResume').click(function(){
-    $('html, body').animate({
-      scrollTop: $("#Resume").offset().top
-    }, 600);
-    return false;
-  });
-  $('.goToContact').click(function(){
-    $('html, body').animate({
-      scrollTop: $("#Contact").offset().top
-    }, 600);
-    return false;
-  });
-
-  var $window = $(window);
 
   $(window).scroll(function () {
+
     if ($window.scrollTop() > 100 && $window.width() > 768)
       $(".nw-navigation").addClass("nw-navbar-shadow");
     else
@@ -41,6 +26,19 @@
       $(".nw-navigation").addClass("nw-navbar-shadow-2x");
     else
       $(".nw-navigation").removeClass("nw-navbar-shadow-2x");
+
+    if ($window.scrollTop() > 400) {
+      $('#card-content').css('visibility', 'visible');
+      $('#card-content').addClass('animated fadeInUp');
+    }
+
+    if ($window.scrollTop() > 1500) {
+      $('.section-contact-container').css('visibility', 'visible');
+      $('.section-contact-container').addClass('animated flipInX');
+    }
+
+    // writePosition();
+
   });
 
   $(window).resize(function () {
@@ -68,6 +66,32 @@
     }
   });
 
+  $('.goToHome').on('click', function () {
+    // $body.animate({
+    //   scrollTop: 0
+    // }, 600);
+    $('html, body').animate({
+      scrollTop: $("#Home").offset().top
+    }, 600);
+
+    return false;
+  });
+
+  $('.goToAbout').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $("#About").offset().top
+    }, 600);
+    return false;
+  });
+
+  $('.goToContact').on('click', function () {
+
+    $('html, body').animate({
+      scrollTop: $("#Contact").offset().top
+    }, 600);
+    return false;
+  });
+
   function initNavigation() {
 
     if ($window.width() < 768) {
@@ -87,7 +111,7 @@
   function initMap() {
     var myLatLng = {lat: 25.058465, lng: 121.5548159};
 
-    var map = new google.maps.Map(document.getElementById('GoogleMap'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 13,
       center: myLatLng,
       draggable: false,
@@ -105,6 +129,40 @@
 
   initMap();
 
-  var wow = new WOW().init();
+  function initSkillBar() {
+    $('.skillbar').each(function () {
+      $(this).find('.skillbar-bar').animate({
+        width: $(this).attr('data-percent')
+      }, 5000);
+    });
+  }
+
+  initSkillBar();
+
+  function init() {
+    $('#card-content').css('visibility', 'hidden');
+    $('.section-contact-container').css('visibility', 'hidden');
+  }
+
+  init();
+
+
+
+  // ------------------------------------------------------------------------
+  // Test Function just to show main variables
+  // ------------------------------------------------------------------------
+  function writePosition() {
+    var html = '';
+    var viewportHeight = $(window).height();
+    var top = $(window).scrollTop();
+    var bottom = top + viewportHeight;
+
+    html += 'viewport size: ' + viewportHeight + '<br>';
+    html += 'top: ' + top + '<br>';
+    html += 'bottom: ' + (top + viewportHeight) + '<br>';
+
+    $('#positionInfo').html(html);
+  }
+
 
 })();
