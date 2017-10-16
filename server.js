0c149850a9e4a6a7081a0825af1acb6dddfe5fbf
Northwalker <northwalker28@gmail.com>
2017-10-16 18:14:18 +0800

    Update devDependencies

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, './dist/index.html'))
+  res.status(200).sendFile(path.join(__dirname, './dist/index.html'))
 })
 
-// Start the server
+// start the server
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   /* eslint-disable no-console */
