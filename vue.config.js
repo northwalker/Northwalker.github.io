541b4d4138a38794892ec0087fd68b623de481fe
Northwalker <northwalker28@gmail.com>
2019-03-02 23:37:56 +0800

    Update html plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].inlineSource = '.(js|css)$'
+        return args
+      })
+  },
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackInlineSourcePlugin()
+    ]
+  },
   css: {
     extract: false
   },
