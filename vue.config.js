02f08c0f270512b41d76f5ef902261ad1e9f1409
Luke Huang <northwalker28@gmail.com>
2019-05-18 00:41:05 +0800

    Update profile (#2)
    * Update config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 // const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      // .tap(args => {
-      //   args[0].inlineSource = '.(js|css)$' // for HtmlWebpackInlineSourcePlugin
-      //   return args
-      // })
-  },
-  configureWebpack: {
-    // plugins: [
-    //   new HtmlWebpackInlineSourcePlugin()
-    // ]
-  },
+  // chainWebpack: config => {
+  //   config
+  //     .plugin('html')
+  //     .tap(args => {
+  //       args[0].inlineSource = '.(js|css)$' // for HtmlWebpackInlineSourcePlugin
+  //       return args
+  //     })
+  // },
+  // configureWebpack: {
+  //   plugins: [
+  //     new HtmlWebpackInlineSourcePlugin()
+  //   ]
+  // },
   css: {
     extract: false
   },
