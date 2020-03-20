64f09b3e33b10465bdd2af7cbd93539a05053eba
Northwalker <northwalker28@gmail.com>
2020-03-20 15:04:52 +0800

    Reset

--- tests/unit/Profile.spec.js
+++
@@ -1,9 +0,0 @@
-
-import Profile from '@/components/Profile.vue'
-jest.mock('@/directives/ImgLoader.js', () => {})
-
-describe('Profile.vue', () => {
-  it('has a created hook', () => {
-    expect(typeof Profile.created).toBe('function')
-  })
-})
