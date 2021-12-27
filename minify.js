55c7117445a5311e1b6fd0689bfdf24dc071e835
Northwalker <northwalker28@gmail.com>
2021-12-27 19:27:15 +0800

    Add version

--- minify.js
+++ minify.js
@@ -11,7 +11,14 @@ try {
     removeAttributeQuotes: true,
     removeComments: true,
   });
-  console.log("HTML minified.", result);
+
+  // TODO: update to build flow
+  result = result.replace(/__VERSION__/g, process.env.version || "4.0.2-beta");
+
+  console.log("========== HTML minified ==========");
+  console.log(result);
+  console.log("========== HTML minified ==========");
+
   fs.writeFileSync("./dist/index.html", result);
 } catch (e) {
   console.log("Error:", e.stack);
