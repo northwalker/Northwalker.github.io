86ba9fbf13fe6f1e3e69f994a12a4f228976448c
Northwalker <northwalker28@gmail.com>
2021-12-29 11:14:28 +0800

    Add test flow

--- script/build.js
+++ script/build.js
@@ -1,3 +1,4 @@
+"use strict";
 var fs = require("fs");
 var path = require("path");
 var minify = require("html-minifier").minify;
---
+++ script/test.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+var path = require("path");
+var amphtmlValidator = require("amphtml-validator");
+
+var htmlString = fs.readFileSync("./public/index.html", "utf8");
+
+amphtmlValidator.getInstance().then(function (validator) {
+  var result = validator.validateString(htmlString);
+  (result.status === "PASS" ? console.log : console.error)(result.status);
+  for (var ii = 0; ii < result.errors.length; ii++) {
+    var error = result.errors[ii];
+    var msg =
+      "line " + error.line + ", col " + error.col + ": " + error.message;
+    if (error.specUrl !== null) {
+      msg += " (see " + error.specUrl + ")";
+    }
+    (error.severity === "ERROR" ? console.error : console.warn)(msg);
+  }
+});
